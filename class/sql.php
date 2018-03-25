1ea7e24986ca58f7b212c90a12f71eb813d717cd
Victor Ibanez <victorviana14@gmail.com>
2018-03-25 17:34:33 -0300

    criamos a classe usuario

--- class/sql.php
+++ class/sql.php
@@ -14,7 +14,7 @@ class Sql extends PDO {
 
         foreach ($parameters as $key => $value) {
 
-            $statment->bindParam($key, $value);
+            $this->setParam($statment, $key, $value);
 
         }
 
